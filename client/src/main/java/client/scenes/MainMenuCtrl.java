bf858d89bf96182b3255b4881f8006295dc757ad
Alexandru Preda <A.Preda-1@student.tudelft.nl>
2022-03-06 16:17:26 +0200

    Removed the unused members from the constructor

--- client/src/main/java/client/scenes/MainMenuCtrl.java
+++ client/src/main/java/client/scenes/MainMenuCtrl.java
@@ -1,15 +1,8 @@
 package client.scenes;
 
-import client.utils.ServerUtils;
-import com.google.inject.Inject;
+
 
 public class MainMenuCtrl {
-    private final ServerUtils server;
-    private final MainCtrl mainCtrl;
-
-    @Inject
-    public MainMenuCtrl(ServerUtils server, MainCtrl mainCtrl) {
-        this.server = server;
-        this.mainCtrl = mainCtrl;
+    public MainMenuCtrl() {
     }
 }
