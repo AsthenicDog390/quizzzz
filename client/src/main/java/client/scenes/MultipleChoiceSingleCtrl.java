266910d7b150eb4341a9bb0c19640f64e097963b
Radu-Stefan Ezaru <R.Ezaru@student.tudelft.nl>
2022-03-19 00:23:13 +0100

    Syncing the timer with the progress bar

--- client/src/main/java/client/scenes/MultipleChoiceSingleCtrl.java
+++ client/src/main/java/client/scenes/MultipleChoiceSingleCtrl.java
@@ -4,6 +4,7 @@ import client.utils.ServerUtils;
 import com.google.inject.Inject;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -18,6 +19,8 @@ public class MultipleChoiceSingleCtrl {
     private Button answerB;
     @FXML
     private Button answerC;
+    @FXML
+    private ProgressBar progressBar;
 
     @Inject
     public MultipleChoiceSingleCtrl(ServerUtils server, MainCtrl mainCtrl) {
@@ -37,12 +40,25 @@ public class MultipleChoiceSingleCtrl {
 
     public void startTimer() {
         Timer gameTimer = new Timer();
+        Timer progressBarTimer = new Timer();
         TimerTask timeOut = new TimerTask() {
             @Override
             public void run() {
                 disableAllButtons();
+                progressBarTimer.cancel();
             }
         };
-        gameTimer.schedule(timeOut,2000);
+
+        TimerTask lowerBar = new TimerTask() {
+            @Override
+            public void run() {
+                double progress = progressBar.getProgress();
+                if(progress>0.004)
+                progressBar.setProgress(progress-0.004);
+            }
+        };
+        gameTimer.schedule(timeOut,10000);
+        progressBarTimer.schedule(lowerBar,0,40);
     }
+
 }
