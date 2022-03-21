7320050a97b7b3157f242c78e8fa8a1942e9c7de
g.konstantinou@student.tudelft.nl <g.konstantinou@student.tudelft.nl>
2022-03-21 17:02:41 +0100

    Cleaned up the code

--- client/src/main/java/client/scenes/WaitingRoomCtrl.java
+++ client/src/main/java/client/scenes/WaitingRoomCtrl.java
@@ -1,6 +1,5 @@
 package client.scenes;
 
-
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
 import javafx.collections.FXCollections;
@@ -9,18 +8,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
-
-
-
 public class WaitingRoomCtrl {
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
 
-
-
     @FXML
     private Label test;
-
 
     @FXML
     private ListView<String> listView;
@@ -34,16 +27,11 @@ public class WaitingRoomCtrl {
         test.setText("Players: " + observableList.size());
     }
 
-
-
-
-
     @Inject
     public WaitingRoomCtrl(ServerUtils server, MainCtrl mainCtrl) {
         this.mainCtrl = mainCtrl;
         this.server = server;
     }
-
     public void showMainMenu() {
         mainCtrl.showMainMenu();
     }
@@ -51,6 +39,4 @@ public class WaitingRoomCtrl {
     public void showStartingScreen() {
         mainCtrl.showStartingScreen();
     }
-
-
 }
