f1cee48e3b4b12036ce1123cd8cc2dc0f60add71
Friso van der Veen <F.B.H.vanderVeen@student.tudelft.nl>
2022-03-27 14:31:32 +0200

    Updated player mode to include score

--- commons/src/test/java/commons/PlayerTest.java
+++ commons/src/test/java/commons/PlayerTest.java
@@ -7,25 +7,25 @@ import static org.junit.jupiter.api.Assertions.*;
 class PlayerTest {
     @Test
     public void testConstructor() {
-        var p = new Player("id", "name", "gameId");
+        var p = new Player("id", "name");
 
         assertEquals("id", p.getId());
         assertEquals("name", p.getName());
-        assertEquals("gameId", p.getGameId());
+        assertEquals("unknown", p.getGameId());
     }
 
     @Test
     public void testEquals() {
-        var p1 = new Player("id", "name", "gameId");
-        var p2 = new Player("id", "name", "gameId");
+        var p1 = new Player("id", "name");
+        var p2 = new Player("id", "name");
 
         assertEquals(p1, p2);
     }
 
     @Test
     public void testNotEquals() {
-        var p1 = new Player("id", "name", "gameId");
-        var p2 = new Player("id", "George", "gameId");
+        var p1 = new Player("id", "name");
+        var p2 = new Player("id", "George");
 
         assertNotEquals(p1, p2);
     }
---
+++ server/src/test/java/server/api/TestPlayerRepository.java
@@ -0,0 +1,160 @@
+package server.api;
+
+import commons.Player;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+import server.database.PlayerRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+public class TestPlayerRepository implements PlayerRepository {
+    @Override
+    public List<Player> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Player> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<Player> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<Player> findAllById(Iterable<String> strings) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(String s) {
+
+    }
+
+    @Override
+    public void delete(Player entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends String> strings) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Player> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public <S extends Player> S save(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends Player> List<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<Player> findById(String s) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(String s) {
+        return false;
+    }
+
+    @Override
+    public void flush() {
+
+    }
+
+    @Override
+    public <S extends Player> S saveAndFlush(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends Player> List<S> saveAllAndFlush(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public void deleteAllInBatch(Iterable<Player> entities) {
+
+    }
+
+    @Override
+    public void deleteAllByIdInBatch(Iterable<String> strings) {
+
+    }
+
+    @Override
+    public void deleteAllInBatch() {
+
+    }
+
+    @Override
+    public Player getOne(String s) {
+        return null;
+    }
+
+    @Override
+    public Player getById(String s) {
+        return null;
+    }
+
+    @Override
+    public <S extends Player> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends Player> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends Player> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public <S extends Player> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends Player> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends Player> boolean exists(Example<S> example) {
+        return false;
+    }
+
+    @Override
+    public <S extends Player, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+        return null;
+    }
+}
