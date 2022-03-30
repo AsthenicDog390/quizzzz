eef3e84e2f7cf2abda92229d04b2052e005a769d
Friso van der Veen <F.B.H.vanderVeen@student.tudelft.nl>
2022-03-30 11:43:16 +0200

    Fixed application hanging when uploading activities

--- client/src/main/java/client/scenes/AddActivitiesCtrl.java
+++ client/src/main/java/client/scenes/AddActivitiesCtrl.java
@@ -2,9 +2,11 @@ package client.scenes;
 
 import client.utils.ActivityUtils;
 import com.google.inject.Inject;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class AddActivitiesCtrl {
     private MainCtrl mainCtrl;
@@ -12,6 +14,9 @@ public class AddActivitiesCtrl {
 
     @FXML
     private TextField fileLocationField;
+
+    @FXML
+    private Text finishedPopup;
 
     @Inject
     public AddActivitiesCtrl(MainCtrl mainCtrl, ActivityUtils activityUtils) {
@@ -34,6 +39,15 @@ public class AddActivitiesCtrl {
             return;
         }
 
-        activityUtils.addActivitiesFromZipFile(loc);
+        finishedPopup.setVisible(false);
+
+        new Thread(() -> {
+            activityUtils.addActivitiesFromZipFile(loc);
+            showFinishedPopup();
+        }).start();
+    }
+
+    void showFinishedPopup() {
+        finishedPopup.setVisible(true);
     }
 }
